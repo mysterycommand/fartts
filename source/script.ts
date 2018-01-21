402bb7495bb71ca43b941ab803a8cb1c84fc88c2
Matt Hayes <matt@mysterycommand.com>
2018-01-21 16:51:33 -0500

    feat(index.html, script.ts, style.scss): add a play/stop button and control for a game-loop

--- source/script.ts
+++ source/script.ts
@@ -1,5 +1,11 @@
 import './style.scss';
 
+const { cancelAnimationFrame: cAF, requestAnimationFrame: rAF } = window;
+
+const playStopButton = document.getElementById('play-stop') as HTMLButtonElement;
+const playLabel = '▶';
+const stopLabel = '■';
+
 const canvas = document.getElementById('canvas') as HTMLCanvasElement;
 const canvasContext = canvas.getContext('2d') as CanvasRenderingContext2D;
 
@@ -16,3 +22,32 @@ canvas.height = buffer.height = h;
 canvasContext.imageSmoothingEnabled = bufferContext.imageSmoothingEnabled = false;
 
 // simulation
+
+// rendering
+
+// game-loop
+let frameId = -1;
+
+function tick(time: number): void {
+  frameId = rAF(tick);
+  console.log(time); // tslint:disable-line
+}
+
+function play(): void {
+  playStopButton.innerText = stopLabel;
+  playStopButton.setAttribute('aria-label', 'stop');
+  frameId = rAF(tick);
+}
+
+function stop(): void {
+  playStopButton.innerText = playLabel;
+  playStopButton.setAttribute('aria-label', 'play');
+  cAF(frameId);
+  frameId = -1;
+}
+
+function toggle(): void {
+  frameId === -1 ? play() : stop();
+}
+
+playStopButton.addEventListener('click', toggle);
