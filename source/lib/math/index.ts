46837c61819b353df1d7ce522f67f22f4b252650
Matt Hayes <matt@mysterycommand.com>
2018-02-04 15:52:26 -0500

    refactor(lib/math/index.ts): adds and uses seedrandom for predictable pseudo-random number generatio

--- source/lib/math/index.ts
+++ source/lib/math/index.ts
@@ -1,2 +1,5 @@
-export const { atan2, cos, hypot, PI: π, random, sin, sqrt } = Math;
+import seedRandom from 'seedrandom';
+
+export const random = seedRandom('fart.ts');
+export const { atan2, cos, hypot, PI: π, sin, sqrt } = Math;
 export const ππ = π * 2;
