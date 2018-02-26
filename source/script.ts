c9009c48ef88cd29148c041db489f4b9c88afd71
Matt Hayes <matt@mysterycommand.com>
2018-02-25 22:46:39 -0500

    refactor(script.ts, images/reginald-teeth-banana.png): add the drop-shadow to the source image, remo

--- source/script.ts
+++ source/script.ts
@@ -227,10 +227,6 @@ function draw(i: number): void {
       bufferContext.save();
 
       bufferContext.translate(x, y);
-      bufferContext.filter = [
-        `drop-shadow(0px 1px 2px rgba(0,0,0,0.3))`,
-        // `hue-rotate(${toDegrees(θ + π)}deg)`,
-      ].join(' ');
       bufferContext.drawImage(
         reginald,
         0,
