c60353c36ca3eac50c049f394d3b945f46237a88
Matt Hayes <matt@mysterycommand.com>
2018-03-13 17:58:24 -0500

    refactor(lib/input/mouse.ts): adds a bunch of bools to the mouse object, updates some properties

--- source/lib/input/mouse.ts
+++ source/lib/input/mouse.ts
@@ -1,3 +1,5 @@
+import Vec2, { clone, sub } from '../geom/vec2';
+
 enum MouseEventType {
   MouseEnter = 'mouseenter',
   MouseLeave = 'mouseleave',
@@ -10,9 +12,29 @@ enum MouseEventType {
 }
 
 export default class Mouse {
-  public static isMouseEvent(event: Event): event is MouseEvent {
-    return event instanceof MouseEvent;
+  public static isWheelEvent(event: Event): event is WheelEvent {
+    return event instanceof WheelEvent;
   }
+
+  public static isMouseEvent(event: Event): event is MouseEvent {
+    return event instanceof MouseEvent || event instanceof WheelEvent;
+  }
+
+  public get cvel(): Vec2 {
+    return sub(this.currPos, this.prevPos);
+  }
+
+  public currPos: Vec2 = new Vec2();
+  public prevPos: Vec2 = new Vec2();
+
+  public isDown: boolean = false;
+
+  public altKey: boolean = false;
+  public ctrlKey: boolean = false;
+  public metaKey: boolean = false;
+  public shiftKey: boolean = false;
+
+  public wheel: Vec2 = new Vec2();
 
   constructor(private context: EventTarget) {
     Object.values(MouseEventType).forEach(type => {
@@ -24,10 +46,30 @@ export default class Mouse {
     event.stopPropagation();
     event.preventDefault();
 
-    if (!Mouse.isMouseEvent(event)) {
-      return;
+    if (Mouse.isMouseEvent(event)) {
+      if (event.type === MouseEventType.MouseEnter) {
+        this.currPos = new Vec2(event.pageX, event.pageY);
+      }
+
+      if (event.type === MouseEventType.MouseDown) {
+        this.isDown = true;
+      }
+
+      if (event.type === MouseEventType.MouseUp) {
+        this.isDown = false;
+      }
+
+      this.prevPos = clone(this.currPos);
+      this.currPos = new Vec2(event.pageX, event.pageY);
+
+      this.altKey = event.altKey;
+      this.ctrlKey = event.ctrlKey;
+      this.metaKey = event.metaKey;
+      this.shiftKey = event.shiftKey;
     }
 
-    console.log(event.type);
+    if (Mouse.isWheelEvent(event)) {
+      this.wheel = new Vec2(event.deltaX, event.deltaY);
+    }
   };
 }
