8d844e8f0e112da180788e487a6383a528f33242
Matt Hayes <matt@mysterycommand.com>
2018-03-13 17:25:17 -0500

    refactor(lib/input/mouse.ts): moves mouse event types to an enum

--- source/lib/input/mouse.ts
+++ source/lib/input/mouse.ts
@@ -1,20 +1,29 @@
+enum MouseEventType {
+  MouseEnter = 'mouseenter',
+  MouseLeave = 'mouseleave',
+  MouseMove = 'mousemove',
+  MouseDown = 'mousedown',
+  MouseUp = 'mouseup',
+  Click = 'click',
+  DblClick = 'dblclick',
+  Wheel = 'wheel',
+}
+
 export default class Mouse {
   public static isMouseEvent(event: Event): event is MouseEvent {
     return event instanceof MouseEvent;
   }
 
   constructor(private context: EventTarget) {
-    context.addEventListener('mouseenter', this.onMouse);
-    context.addEventListener('mouseleave', this.onMouse);
-    context.addEventListener('mousemove', this.onMouse);
-    context.addEventListener('mousedown', this.onMouse);
-    context.addEventListener('mouseup', this.onMouse);
-
-    context.addEventListener('click', this.onMouse);
-    context.addEventListener('dblclick', this.onMouse);
+    Object.values(MouseEventType).forEach(type => {
+      context.addEventListener(type, this.onMouse);
+    });
   }
 
   private onMouse = (event: Event) => {
+    event.stopPropagation();
+    event.preventDefault();
+
     if (!Mouse.isMouseEvent(event)) {
       return;
     }
