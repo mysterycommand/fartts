9911c61cf2880db16a6ec93b56a545d6ffb16c94
Matt Hayes <matt@mysterycommand.com>
2018-03-15 07:50:46 -0500

    Inputs (#18)
    * feat(lib/input/mouse.ts): adds an initial mouse listener
    
    * chore(tsconfig.json): updates the libs to include es7 and es8 stuff
    
    * refactor(lib/input/mouse.ts): moves mouse event types to an enum
    
    * refactor(lib/input/mouse.ts): adds a bunch of bools to the mouse object, updates some properties
    
    * refactor(lib/input/mouse.ts, lib/input/keyboard.ts, script.ts): initial keyboard implementation, start turning mouse into an event target
    
    * refactor(lib/input/keyboard.ts, lib/input/mouse.ts, script.ts): fixes some style issues in mouse, an
    
    * refactor(lib/input/keyboard.ts, lib/input/mouse.ts, script.ts): updates keyboard rendering to reflec
    
    * test(lib/math.test.ts, lib/input/*.ts): add initial tests for new classes
    
    * test(package.json, lib/input/mouse*): add tests for public static members, shuffle some tasks around
    
    * test(lib/input/*): add test coverage to keyboard and mouse watchers

--- source/script.ts
+++ source/script.ts
@@ -1,18 +1,13 @@
 import './style.scss';
 
-import Vec2, { add, fromPolar, lerp, limit, normalize, scale, sub } from './lib/geom/vec2';
+import Keyboard, { KeyCode } from './lib/input/keyboard';
+import Mouse from './lib/input/mouse';
 import { floor, min, random, round, toDegrees, π, ππ } from './lib/math';
-import Particle from './lib/physics/particle';
 
-import reginaldUrl from './images/reginald-teeth-banana.png';
-const reginald = new Image(128, 1024);
-const rs = 3;
+const keyboard = new Keyboard(document);
+const mouse = new Mouse(document);
 
 const { cancelAnimationFrame: cAF, requestAnimationFrame: rAF } = window;
-
-// const playStopButton = document.getElementById('play-stop') as HTMLButtonElement;
-// const playLabel = '▶';
-// const stopLabel = '■';
 
 const canvas = document.getElementById('canvas') as HTMLCanvasElement;
 const canvasContext = canvas.getContext('2d') as CanvasRenderingContext2D;
@@ -20,309 +15,185 @@ const canvasContext = canvas.getContext('2d') as CanvasRenderingContext2D;
 const buffer = document.createElement('canvas') as HTMLCanvasElement;
 const bufferContext = buffer.getContext('2d') as CanvasRenderingContext2D;
 
-const stageScale = 2;
+const stageScale = 1;
 const stageWidth = canvas.clientWidth / stageScale;
 const stageHeight = canvas.clientHeight / stageScale;
-const centerX = stageWidth / 2;
-const centerY = stageHeight / 2;
+const stageCenterX = stageWidth / 2;
+const stageCenterY = stageHeight / 2;
 
 canvas.width = buffer.width = stageWidth;
 canvas.height = buffer.height = stageHeight;
 canvasContext.imageSmoothingEnabled = bufferContext.imageSmoothingEnabled = false;
 
-/**
- * SIMULATION
- */
+const rows = [
+  [
+    KeyCode.Escape,
+    KeyCode.None,
+    KeyCode.None,
+    KeyCode.None,
+    KeyCode.None,
+    KeyCode.None,
+    KeyCode.None,
+    KeyCode.None,
+    KeyCode.None,
+    KeyCode.None,
+    KeyCode.None,
+    KeyCode.None,
+    KeyCode.None,
+    KeyCode.None,
+  ],
+  [
+    KeyCode.Backquote,
+    KeyCode.Digit1,
+    KeyCode.Digit2,
+    KeyCode.Digit3,
+    KeyCode.Digit4,
+    KeyCode.Digit5,
+    KeyCode.Digit6,
+    KeyCode.Digit7,
+    KeyCode.Digit8,
+    KeyCode.Digit9,
+    KeyCode.Digit0,
+    KeyCode.Minus,
+    KeyCode.Equal,
+    KeyCode.Backspace,
+  ],
+  [
+    KeyCode.Tab,
+    KeyCode.KeyQ,
+    KeyCode.KeyW,
+    KeyCode.KeyE,
+    KeyCode.KeyR,
+    KeyCode.KeyT,
+    KeyCode.KeyY,
+    KeyCode.KeyU,
+    KeyCode.KeyI,
+    KeyCode.KeyO,
+    KeyCode.KeyP,
+    KeyCode.BracketLeft,
+    KeyCode.BracketRight,
+    KeyCode.Backslash,
+  ],
+  [
+    KeyCode.CapsLock,
+    KeyCode.KeyA,
+    KeyCode.KeyS,
+    KeyCode.KeyD,
+    KeyCode.KeyF,
+    KeyCode.KeyG,
+    KeyCode.KeyH,
+    KeyCode.KeyJ,
+    KeyCode.KeyK,
+    KeyCode.KeyL,
+    KeyCode.Semicolon,
+    KeyCode.Quote,
+    KeyCode.Enter,
+  ],
+  [
+    KeyCode.ShiftLeft,
+    KeyCode.KeyZ,
+    KeyCode.KeyX,
+    KeyCode.KeyC,
+    KeyCode.KeyV,
+    KeyCode.KeyB,
+    KeyCode.KeyN,
+    KeyCode.KeyM,
+    KeyCode.Comma,
+    KeyCode.Period,
+    KeyCode.Slash,
+    KeyCode.ShiftRight,
+  ],
+  [
+    KeyCode.None,
+    KeyCode.ControlLeft,
+    KeyCode.AltLeft,
+    KeyCode.MetaLeft,
+    KeyCode.Space,
+    KeyCode.MetaRight,
+    KeyCode.AltRight,
+    KeyCode.ArrowLeft,
+    KeyCode.ArrowUp,
+    KeyCode.ArrowRight,
+    KeyCode.ArrowDown,
+  ],
+];
 
-// TODO: factor this out into a constant force behavior
-// const g = new Vec2(0, 0.15);
-// const gravity = () => g;
+const mostColumns = rows.reduce((cols, { length }) => (length > cols ? length : cols), 0);
 
-// TODO: factor this out into a drag behavior
-// const d = 0.01;
-// const drag = (p: Particle) => scale(lerp(p.cvel, Vec2.zero, d), -1);
+const keySize = stageWidth / mostColumns;
 
-const speed = 3 / stageScale;
-const force = 0.025 / stageScale;
-
-function getSeparate(ps: Particle[], dist: number = 50 / stageScale): (p: Particle) => Vec2 {
-  return (p: Particle): Vec2 => {
-    let j = 0;
-
-    let w = ps.reduce((v, q) => {
-      const d = sub(p.cpos, q.cpos);
-
-      if (d.ρ > 0 && d.ρ < dist) {
-        ++j;
-        return add(v, scale(scale(d, 1 / d.ρ), 1 / d.ρ));
-      }
-
-      return v;
-    }, Vec2.zero);
-
-    if (j > 0) {
-      w = scale(w, 1 / j);
-    }
-
-    if (w.ρ > 0) {
-      return scale(limit(sub(scale(normalize(w), speed), p.cvel), force), 1.5);
-    }
-
-    return w;
-  };
-}
-
-function getAlign(ps: Particle[], dist: number = 100 / stageScale): (p: Particle) => Vec2 {
-  return (p: Particle): Vec2 => {
-    let j = 0;
-
-    let w = ps.reduce((v, q) => {
-      const d = sub(p.cpos, q.cpos);
-
-      if (d.ρ > 0 && d.ρ < dist) {
-        ++j;
-        return add(v, q.cvel);
-      }
-
-      return v;
-    }, Vec2.zero);
-
-    if (j > 0) {
-      w = scale(w, 1 / j);
-      return limit(sub(scale(normalize(w), speed), p.cvel), force);
-    }
-
-    return Vec2.zero;
-  };
-}
-
-function getCohere(ps: Particle[], dist: number = 100 / stageScale): (p: Particle) => Vec2 {
-  return (p: Particle): Vec2 => {
-    let j = 0;
-
-    let w = ps.reduce((v, q) => {
-      const d = sub(p.cpos, q.cpos);
-
-      if (d.ρ > 0 && d.ρ < dist) {
-        ++j;
-        return add(v, q.cpos);
-      }
-
-      return v;
-    }, Vec2.zero);
-
-    if (j > 0) {
-      w = scale(w, 1 / j);
-      return limit(sub(scale(normalize(sub(w, p.cpos)), speed), p.cvel), force);
-    }
-
-    return Vec2.zero;
-  };
-}
-
-const numParticles = 20;
-const particles: Particle[] = [];
-
-const separate = getSeparate(particles);
-const align = getAlign(particles);
-const cohere = getCohere(particles);
-
-const origin = new Vec2(centerX, centerY);
-
-function init() {
-  // const initX = stageWidth * 0.2 + random() * stageWidth * 0.6;
-  // const initY = stageHeight * 0.2 + random() * stageHeight * 0.6;
-
-  for (let i = 0; i < numParticles; ++i) {
-    const cpos = add(origin, fromPolar(i / numParticles * ππ, random() * 20));
-    const ppos = add(cpos, fromPolar(random() * ππ, 1));
-
-    const particle = new Particle(cpos, ppos);
-    particles.push(particle);
-
-    // particle.behaviors.push(gravity);
-    // particle.behaviors.push(drag);
-
-    particle.behaviors.push(separate);
-    particle.behaviors.push(align);
-    particle.behaviors.push(cohere);
-  }
-}
-
-/**
- * update: updates the simulation
- * @param t {number} - the number of miliseconds that have ellapsed since start
- * @param dt {number} - the number of miliseconds to simulate
- */
-function update(t: number, dt: number): void {
-  if (particles.length === 0) {
-    init();
-  }
-
-  particles.forEach(particle => {
-    // console.log(t); // tslint:disable-line
-    particle.update(t, dt);
-
-    const { cpos: { x, y }, cvel } = particle;
-    let move = Vec2.zero;
-
-    if (y < 0 - (128 - 96) / rs) {
-      move = new Vec2(0, stageHeight + 128 / rs);
-      particle.ppos = add(particle.ppos, move);
-      particle.cpos = add(particle.cpos, move);
-    }
-
-    if (x < -64 / rs) {
-      move = new Vec2(stageWidth + 128 / rs, 0);
-      particle.ppos = add(particle.ppos, move);
-      particle.cpos = add(particle.cpos, move);
-    }
-
-    if (x > stageWidth + 64 / rs) {
-      move = new Vec2(stageWidth + 128 / rs, 0);
-      particle.ppos = sub(particle.ppos, move);
-      particle.cpos = sub(particle.cpos, move);
-    }
-
-    if (y > stageHeight + 96 / rs) {
-      move = new Vec2(0, stageHeight + 128 / rs);
-      particle.ppos = sub(particle.ppos, move);
-      particle.cpos = sub(particle.cpos, move);
-    }
-  });
-
-  // particles = particles.filter(({ cpos: { y } }) => y < stageHeight);
-  // particles = particles.filter(({ cpos: { x, y } }) => {
-  //   const top = 0 < y;
-  //   const right = x < stageWidth;
-  //   const bottom = y < stageHeight;
-  //   const left = 0 < x;
-  //   return top && right && bottom && left;
-  // });
-}
-
-/**
- * RENDERING
- */
-
-/**
- * draw: renders a frame to the canvas
- * @param i {number} - the 'interpolation percentage' is the amount of time that
- * has yet to be simulated this frame as a percentage of the simulation step
- */
-function draw(i: number): void {
-  bufferContext.fillStyle = 'gray';
+function draw(/* i: number */): void {
+  bufferContext.fillStyle = '#666';
   bufferContext.fillRect(0, 0, stageWidth, stageHeight);
 
-  const start = performance.now();
-  particles
-    .sort((a, b) => {
-      return a.ipos(i).y - b.ipos(i).y;
-    })
-    .some(particle => {
-      const { cvel: { θ, ρ } } = particle;
-      const { x, y } = particle.ipos(i);
-      const frame = floor(((round(toDegrees(θ + π)) + 22.5) % 360) / 45);
+  bufferContext.strokeStyle = '#fff';
 
-      bufferContext.save();
+  let keyY = 5;
+  rows.forEach((row, i) => {
+    let keyX = 5;
+    let keyHeight = i === 0 ? (keySize - 12) / 2 : keySize - 12;
 
-      bufferContext.translate(x, y);
-      bufferContext.drawImage(
-        reginald,
-        0,
-        128 * frame,
-        128,
-        128,
-        -64 / rs,
-        -96 / rs,
-        128 / rs,
-        128 / rs,
-      );
+    row.forEach((col, j) => {
+      bufferContext.fillStyle = col === KeyCode.None ? '#aaa' : '#fff';
 
-      bufferContext.restore();
+      let keyWidth;
+      switch (col) {
+        case KeyCode.None:
+          keyWidth = keySize - 10;
+          break;
+        case KeyCode.Backspace:
+        case KeyCode.Tab:
+          keyWidth = keySize + 14;
+          break;
+        case KeyCode.CapsLock:
+        case KeyCode.Enter:
+          keyWidth = keySize + 30;
+          break;
+        case KeyCode.ShiftLeft:
+        case KeyCode.ShiftRight:
+          keyWidth = keySize + 60;
+          break;
+        case KeyCode.Space:
+          keyWidth = keySize + 248;
+          break;
+        default:
+          keyWidth = keySize - 12;
+          break;
+      }
 
-      return performance.now() - start > step / 2;
+      if (
+        col === KeyCode.ArrowLeft ||
+        col === KeyCode.ArrowUp ||
+        col === KeyCode.ArrowRight ||
+        col === KeyCode.ArrowDown
+      ) {
+        keyHeight = (keySize - 12) / 2;
+        keyY = (keySize - 10) * rows.length - 8;
+
+        if (col === KeyCode.ArrowUp) {
+          keyY -= keyHeight;
+        }
+
+        if (col === KeyCode.ArrowDown) {
+          keyX -= (keyWidth + 10) * 2;
+        }
+      }
+
+      keyboard.keysDown[col] || col === KeyCode.None
+        ? bufferContext.fillRect(keyX, keyY, keyWidth, keyHeight)
+        : bufferContext.strokeRect(keyX, keyY, keyWidth, keyHeight);
+
+      keyX += keyWidth + 10;
     });
+    keyY += keyHeight + 10;
+  });
 
   canvasContext.drawImage(buffer, 0, 0);
 }
 
-/**
- * GAME-LOOP
- */
-const step = 1000 / 60;
-let excess = 0;
-
-let frameId = -1;
-
-// resets everytime you click 'play'
-let firstTime = 0;
-let previousTime = 0;
-
-// calculated each frame, relative to first/previous frame (respectively)
-let normalTime = 0;
-let deltaTime = 0;
-
-function tick(time: number): void {
-  frameId = rAF(tick);
-
-  normalTime = time - firstTime;
-  deltaTime = normalTime - previousTime;
-
-  previousTime = normalTime;
-  excess += deltaTime;
-
-  excess = min(excess, 1000);
-  while (excess >= step) {
-    update(normalTime, step);
-    excess -= step;
-  }
-
-  draw(excess / step);
+function tick(): void {
+  rAF(tick);
+  draw();
 }
 
-function play(): void {
-  // playStopButton.innerText = stopLabel;
-  // playStopButton.setAttribute('aria-label', 'stop');
-
-  frameId = rAF((time: number) => {
-    firstTime = time;
-    previousTime = 0;
-    excess = 0;
-
-    frameId = rAF(tick);
-  });
-}
-
-function stop(): void {
-  // playStopButton.innerText = playLabel;
-  // playStopButton.setAttribute('aria-label', 'play');
-
-  cAF(frameId);
-  frameId = -1;
-}
-
-function goto(f: number): void {
-  normalTime = 0;
-
-  for (let i = 0; i < f; ++i) {
-    normalTime += step;
-    update(normalTime, step);
-  }
-
-  draw(1);
-}
-
-function toggle(): void {
-  frameId === -1 ? play() : stop();
-}
-
-// playStopButton.addEventListener('click', toggle);
-// goto(3);
-// play();
-
-reginald.addEventListener('load', event => {
-  play();
-});
-
-reginald.src = reginaldUrl;
+tick();
