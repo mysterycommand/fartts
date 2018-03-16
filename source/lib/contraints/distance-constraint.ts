e9ee58b36c00b64600b1fcd5a29f06e10122c40a
Matt Hayes <matt@mysterycommand.com>
2018-03-15 23:47:53 -0500

    refactor(lib/contraints/distance-constraint.ts): refactors for clarity (I hope)

--- source/lib/contraints/distance-constraint.ts
+++ source/lib/contraints/distance-constraint.ts
@@ -7,20 +7,21 @@ export default class DistanceConstraint implements IConstraint {
     public a: Particle,
     public b: Particle,
     public stiffness: number = 5,
-    public distance: number = sub(a.currPos, b.currPos).ρ,
+    public restLength: number = sub(a.currPos, b.currPos).ρ,
   ) {}
 
   public update(t: number, dt: number): void {
     const stepCoef = 1 / dt;
 
-    const normal = sub(this.a.currPos, this.b.currPos);
-    const m = normal.ρ * normal.ρ;
-    const s = scale(normal, (this.distance * this.distance - m) / m * this.stiffness * stepCoef);
+    const delta = sub(this.a.currPos, this.b.currPos);
+    const length = delta.ρ + Number.EPSILON; // avoids division by 0
+    const restitution = (this.restLength - length) / length * this.stiffness * stepCoef;
+    const adjustment = scale(delta, restitution);
 
-    this.a.currPos.x += s.x;
-    this.a.currPos.y += s.y;
+    this.a.currPos.x += adjustment.x;
+    this.a.currPos.y += adjustment.y;
 
-    this.b.currPos.x -= s.x;
-    this.b.currPos.y -= s.y;
+    this.b.currPos.x -= adjustment.x;
+    this.b.currPos.y -= adjustment.y;
   }
 }
