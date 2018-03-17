c008762d06e457782367db53d1cc8a1c695f0e78
Matt Hayes <matt@mysterycommand.com>
2018-03-17 11:16:35 -0600

    refactor(script.ts): draw the limbs with code (easier registration), tweak the configs

--- source/script.ts
+++ source/script.ts
@@ -30,16 +30,6 @@ import { lKnee, rKnee } from './game/puppet/knees';
 import { lShoulder, rShoulder } from './game/puppet/shoulders';
 import { lWrist, rWrist } from './game/puppet/wrists';
 
-import lCalfSrc from './images/moon-body/small/l-calf.png';
-import lFootSrc from './images/moon-body/small/l-foot.png';
-import lForearmSrc from './images/moon-body/small/l-forearm.png';
-import lThighSrc from './images/moon-body/small/l-thigh.png';
-import lUpperSrc from './images/moon-body/small/l-upper-arm.png';
-import rCalfSrc from './images/moon-body/small/r-calf.png';
-import rFootSrc from './images/moon-body/small/r-foot.png';
-import rForearmSrc from './images/moon-body/small/r-forearm.png';
-import rThighSrc from './images/moon-body/small/r-thigh.png';
-import rUpperSrc from './images/moon-body/small/r-upper-arm.png';
 import facesSrc from './images/moon-faces-small.png';
 import phasesSrc from './images/moon-phases-small.png';
 
@@ -77,6 +67,8 @@ function update(t: number, dt: number): void {
  * RENDERER
  */
 
+const thickness = 20;
+
 /**
  * draw: renders a frame to the canvas
  * @param i {number} - the 'interpolation percentage' is the amount of time that
@@ -94,66 +86,61 @@ function draw(i: number): void {
 
   [
     {
-      from: rAnkle,
-      to: rKnee,
-      img: images.rFoot,
-    },
-    {
-      from: lAnkle,
-      to: lKnee,
-      img: images.lFoot,
-    },
-    {
       from: rElbow,
       to: rWrist,
-      img: images.rForearm,
     },
     {
       from: lElbow,
       to: lWrist,
-      img: images.lForearm,
     },
     {
       from: rHip,
       to: rKnee,
-      img: images.rThigh,
     },
     {
       from: lHip,
       to: lKnee,
-      img: images.lThigh,
     },
     {
       from: rKnee,
       to: rAnkle,
-      img: images.rCalf,
     },
     {
       from: lKnee,
       to: lAnkle,
-      img: images.lCalf,
     },
     {
       from: rShoulder,
       to: rElbow,
-      img: images.rUpper,
     },
     {
       from: lShoulder,
       to: lElbow,
-      img: images.lUpper,
     },
-  ].forEach(({ from, to, img }) => {
+  ].forEach(({ from, to }) => {
+    const diff = sub(from.currPos, to.currPos);
+
     bufferContext.save();
-    bufferContext.translate(from.currPos.x - img.width / 2, from.currPos.y);
-    let angle = sub(from.currPos, to.currPos).θ;
-    if (img === images.lFoot || img === images.rFoot) {
-      angle -= π / 2;
-    } else {
-      angle += π / 2;
-    }
-    bufferContext.rotate(angle);
-    bufferContext.drawImage(img, 0, 0);
+    bufferContext.translate(from.currPos.x, from.currPos.y);
+    bufferContext.rotate(diff.θ + π / 2);
+
+    bufferContext.fillStyle = '#d8d8d8';
+
+    bufferContext.beginPath();
+    bufferContext.rect(-thickness / 2, 0, thickness, diff.ρ);
+    bufferContext.closePath();
+    bufferContext.fill();
+
+    bufferContext.beginPath();
+    bufferContext.arc(0, 0, thickness / 2, 0, ππ);
+    bufferContext.closePath();
+    bufferContext.fill();
+
+    bufferContext.beginPath();
+    bufferContext.arc(0, diff.ρ, thickness / 2, 0, ππ);
+    bufferContext.closePath();
+    bufferContext.fill();
+
     bufferContext.restore();
   });
 
@@ -266,16 +253,6 @@ document.addEventListener('keyup', event => {
 
 Promise.all(
   Object.entries({
-    lCalf: lCalfSrc,
-    lFoot: lFootSrc,
-    lForearm: lForearmSrc,
-    lThigh: lThighSrc,
-    lUpper: lUpperSrc,
-    rCalf: rCalfSrc,
-    rFoot: rFootSrc,
-    rForearm: rForearmSrc,
-    rThigh: rThighSrc,
-    rUpper: rUpperSrc,
     faces: facesSrc,
     phases: phasesSrc,
   }).map(
