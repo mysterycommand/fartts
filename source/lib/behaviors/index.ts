18d915e0a89a780250f88dee354bef7e536c31e0
Matt Hayes <matt@mysterycommand.com>
2018-04-27 16:53:37 -0400

    A New Beginning (#38)
    * chore(package, lock): yarn upgrade -LE
    
    * refactor(source): the beginning of a ground up rewrite

--- source/lib/behaviors/index.ts
+++
@@ -1,6 +0,0 @@
-import Vec2 from '../geom/vec2';
-import Particle from '../physics/particle';
-
-export interface IBehavior {
-  update(p: Particle, t: number, dt: number): Vec2;
-}
--- source/lib/math.test.ts
+++ source/lib/math.test.ts
@@ -1,9 +1,28 @@
-import { randomBool, toDegrees, toRadians, π, ππ } from './math';
+import { lerp, randomBool, saw, toDegrees, toRadians, tri, π, ππ } from './math';
+
+test('lerp', () => {
+  expect(lerp(1, 2, 0.5)).toBe(1.5);
+  expect(lerp(0, 2, 0.5)).toBe(1);
+  expect(lerp(1, 5, 0.25)).toBe(2);
+});
 
 test('randomBool', () => {
   expect(randomBool()).toBe(false);
   expect(randomBool()).toBe(false);
   expect(randomBool()).toBe(true);
+});
+
+test('saw', () => {
+  expect(saw(0)).toBe(-1);
+  expect(saw(π)).toBe(0);
+  expect(saw(ππ * 0.999)).toBeCloseTo(1);
+  expect(saw(ππ)).toBe(-1); // at ππ it flips back down, sawtooth-style
+});
+
+test('tri', () => {
+  expect(tri(0)).toBe(-1);
+  expect(tri(π / 2)).toBe(0);
+  expect(tri(π)).toBe(1);
 });
 
 test('toDegrees', () => {
